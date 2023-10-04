a4b36e3e78e7e4d013546e0af0ecf6f279c464d1
Washio Ferdous Rubai <66668097+wf-rubai@users.noreply.github.com>
2023-10-04 18:12:31 +0600

    Update UIGenerator.java

--- src/DevMain/UIGenerator.java
+++ src/DevMain/UIGenerator.java
@@ -1,17 +1,34 @@
 package DevMain;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class UIGenerator extends JFrame{
+    
+    private JPanel pSB = new JPanel();
+
     public UIGenerator(String s){
+
         super(s);
         setSize(1250, 850);
         setLayout(null);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBackground(Color.gray);
 
+        pSB.setBounds(0, 0, 70, 850);
+        pSB.setBackground(Color.darkGray);
+        pSB.setLayout(null);
+
+        add(pSB);
         add(new MapView().panel());
 
         setVisible(true);
+    }
+
+    public UIGenerator(){
+
     }
 }
